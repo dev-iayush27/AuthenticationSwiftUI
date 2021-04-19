e7458a79d1b89d2358de9f9956043d51dc11dce7
Ayush Gupta <ayush.gupta2@cognizant.com>
2021-04-19 13:00:52 +0530

    Initial Commit

---
+++ AuthenticationSwiftUI/AuthenticationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationSwiftUIApp.swift
+//  AuthenticationSwiftUI
+//
+//  Created by M_AMBIN03921 on 19/04/21.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthenticationSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AuthenticationSwiftUI
+//
+//  Created by M_AMBIN03921 on 19/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
